cb2eb8e5ee3e03c394969c2f3497f9d8f8101df7
Bhat Shaqran <bhatshakran@gmail.com>
2021-08-29 11:08:57 +0530

    Player movement added

--- main.js
+++ main.js
@@ -58,8 +58,40 @@ scene("game", () => {
     "#": [sprite("mushroom", solid())],
   };
 
+  // game level instantiation
   const gameLevel = addLevel(map, levelCfg);
+
+  // score
+  const scoreLabel = add([
+    text("score"),
+    pos(30, 6),
+    layer("ui"),
+    {
+      value: "score",
+    },
+  ]);
+
+  add([text("level" + "test", pos(4, 6))]);
+
+  // player instantiation
   const player = add([sprite("mario"), pos(30, 0), origin("bot"), body()]);
+
+  // Player movement
+
+  const MOVE_SPEED = 120;
+  const JUMP_FORCE = 280;
+
+  keyDown("right", () => {
+    player.move(MOVE_SPEED, 0);
+  });
+  keyDown("left", () => {
+    player.move(-MOVE_SPEED, 0);
+  });
+  keyPress("space", () => {
+    if (player.grounded()) {
+      player.jump(JUMP_FORCE);
+    }
+  });
 });
 
 start("game");

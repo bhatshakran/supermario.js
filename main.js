b161bedfa278b1f241713e472572653492fafabe
Bhat Shaqran <bhatshakran@gmail.com>
2021-08-29 10:13:08 +0530

    First scene created

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ kaboom({
   global: true,
   fullscreen: true,
   scale: 1,
+  width: 2000,
+  height: 3000,
   debug: true,
   clearColor: [0, 0, 0, 1],
 });
@@ -12,9 +14,37 @@ loadSprite("evil-shroom", "evilshroom.png");
 loadSprite("brick", "brick.png");
 loadSprite("block", "stone.png");
 loadSprite("mario", "mario.png");
+loadSprite("mushroom", "mushroom.png");
+loadSprite("surpise", "surprisebox.png");
+loadSprite("unboxed", "openedbox.png");
+loadSprite("pipe-top-left", "topleft.png");
+loadSprite("pipe-top-right", "topright.png");
+loadSprite("pipe-bottom-left", "bottomleft.png");
+loadSprite("pipe-bottom-right", "bottomright.png");
 
 scene("game", () => {
   layers(["bg", "obj", "ui"], "obj");
+
+  const map = [
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    "                            ",
+    "======================== ===",
+  ];
+
+  const levelCfg = {
+    width: 20,
+    height: 20,
+    "=": [sprite("block", solid())],
+  };
+
+  const gameLevel = addLevel(map, levelCfg);
 });
 
 go("game");
